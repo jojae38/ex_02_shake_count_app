782fd50addbd67c7fc05495b61d7d7650970b688
jojae <jojae38@naver.com>
2023-11-20 09:27:54 +0900

    flutter native screen 추가

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shake/shake.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -19,18 +20,32 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+  //with 대신 implement를 사용할 수도 있지만 대신 필수로 구현해야 하는 함수를 그대로 구현시켜줘야 한다.
   int _counter = 0;
+  late ShakeDetector detector;
+
+  //나중에 초기화가 될 것이기에 late로 선언
+  //didChangeAppLifecycleState에서 사용하기위해 initstate에서 선언부를 꺼냄
+
   @override
-    void initState() {
-      super.initState();
-      ShakeDetector detector = ShakeDetector.autoStart(onPhoneShake: (){
-        setState(() {
-          _counter++;
-        });
-      },shakeThresholdGravity: 1.5);
-      //민감도 값 변경
-    }
+  void initState() {
+    super.initState();
+    detector = ShakeDetector.autoStart(
+        onPhoneShake: () {
+          setState(() {
+            _counter++;
+          });
+        },
+        shakeThresholdGravity: 1.5);
+    //민감도 값 변경
+    WidgetsBinding.instance.addObserver(this);
+    //옵저버라는 리스너 추가
+    //addObserver 내에 관찰할 객체(대상)을 넣어줘야 함 -> WidgetsBindingObserver
+    //그러나 _MyHomePageState는 WidgestBindingObserver가 아니기에 with를 이용해 해당 객체로 변신을 시켜줍니다.
+    //mixing 형태로 구현했다고 생각하면 편하다.
+  }
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -43,45 +58,58 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.detached:
+        break;
+      case AppLifecycleState.resumed:
+        detector.startListening();
+        break;
+      case AppLifecycleState.inactive:
+        break;
+      case AppLifecycleState.paused:
+        detector.stopListening();
+        break;
+      case AppLifecycleState.hidden:
+        break;
+    }
+  }
+
+  //각 경우의 수에 따른 detector의 활동을 중지 혹은 재시작 설정
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    //이것을 하지 않으면 메모리 reak이 생길 수 있다.
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              '흔들어서 카운트를 올려보세요',
-            ),
+            '흔들어서 카운트를 올려보세요'
+                .text
+                .bold
+                .indigo300
+                .isIntrinsic
+                .size(20)
+                .makeCentered()
+                .box
+                .color(Colors.black)
+                .height(40)
+                .withRounded()
+                .make()
+                .pSymmetric(h: 10, v: 10),
+            //velocity_x를 이용해 Text랑 패딩 그리고 감싼 box를 축약한 코드
+            //이걸 이제 메소드나 위젯 형태로 Extract해서 관리할 것!
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.displayLarge,
